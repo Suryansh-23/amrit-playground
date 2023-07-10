18decffaa3ec2b8afb21cc3109dfcd166069b02f
Suryansh Chauhan <suryanshchauhan2005@gmail.com>
2023-07-11 02:54:44 +0530

    seo stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,62 @@
-"use client";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
-import { AnimatePresence, motion } from "framer-motion";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Amrit - Programming now in हिन्दी",
+    description:
+        "Why not code in your mother tongue हिन्दी, make it easy for everyone to learn programming.",
+    generator: "Next.js",
+    applicationName: "Amrit Playground",
+    referrer: "origin-when-cross-origin",
+    keywords: [
+        "Amrit",
+        "Programming",
+        "Hindi",
+        "हिन्दी",
+        "Devanagari",
+        "देवनागरी",
+        "Programming in Hindi",
+        "Programming in हिन्दी",
+        "Programming in Devanagari",
+        "Programming in देवनागरी",
+        "Suryansh",
+        "Chauhan",
+    ],
+    authors: {
+        name: "Suryansh Chauhan",
+        url: "https://github.com/Suryansh-23/",
+    },
+    colorScheme: "light",
+    creator: "Suryansh Chauhan",
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    openGraph: {
+        type: "website",
+        locale: "en_IN",
+        url: "https://amrit-playground.vercel.app/",
+        title: "Amrit - Programming now in हिन्दी",
+        description:
+            "Why not code in your mother tongue हिन्दी, make it easy for everyone to learn programming.",
+        siteName: "Amrit Playground",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Amrit - Programming now in हिन्दी",
+        description:
+            "Why not code in your mother tongue हिन्दी, make it easy for everyone to learn programming.",
+        creator: "@SuriPuri23",
+        creatorId: "947709391750307840",
+    },
+};
 
 export default function RootLayout({
     children,
@@ -16,30 +65,13 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <Script
-                src="/wasm_exec.js"
-                onLoad={() => {
-                    if (!WebAssembly) {
-                        console.warn(
-                            "WebAssembly is not supported in your browser"
-                        );
-                    }
-                }}
-            />
             <body className={inter.className}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="light"
                     enableSystem>
-                    <AnimatePresence>
-                        <motion.div
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}>
-                            {children}
-                            <Toaster />
-                        </motion.div>
-                    </AnimatePresence>
+                    {children}
+                    <Toaster />
                 </ThemeProvider>
                 <Analytics />
             </body>
--- app/page.tsx
+++ app/page.tsx
@@ -4,41 +4,63 @@ import "@/app/globals.css";
 import Hero from "@/components/hero";
 import Image from "next/image";
 
+import Navbar from "@/components/navbar";
 import Playground from "@/components/playground";
 import highlight from "@/public/highlight.svg";
+import { AnimatePresence, motion } from "framer-motion";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism.css";
-import Navbar from "@/components/navbar";
+import Script from "next/script";
 
 export default function Home() {
     return (
         <>
-            <Navbar />
-            <Hero />
-            <main id="playground" className="mt-20">
-                <h1 className=" mb-10 text-center font-display text-stone-700 font-display text-3xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[6rem] relative">
-                    C&apos;mon
-                    <span className="relative mx-8">
-                        try
-                        <Image
-                            width={100}
-                            className="absolute z-10 w-40 origin-center top-1/2 left-1/2 opacity-80 select-none"
+            <Script
+                src="/wasm_exec.js"
+                onLoad={() => {
+                    if (!WebAssembly) {
+                        console.warn(
+                            "WebAssembly is not supported in your browser"
+                        );
+                    }
+                }}
+            />
+            <AnimatePresence>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}>
+                    <Navbar />
+                    <Hero />
+                    <main id="playground" className="mt-20">
+                        <h1 className=" mb-10 text-center font-display text-stone-700 font-display text-3xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[6rem] relative">
+                            C&apos;mon
+                            <span className="relative mx-8">
+                                try
+                                <Image
+                                    width={100}
+                                    className="absolute z-10 w-40 origin-center top-1/2 left-1/2 opacity-80 select-none"
+                                    style={{
+                                        transform:
+                                            "translate(-50%, -40%) scale(1.75)",
+                                    }}
+                                    src={highlight}
+                                    alt={""}
+                                />
+                            </span>
+                            it out!
+                        </h1>
+                        <div
+                            className="py-20"
                             style={{
-                                transform: "translate(-50%, -40%) scale(1.75)",
-                            }}
-                            src={highlight}
-                            alt={""}
-                        />
-                    </span>
-                    it out!
-                </h1>
-                <div
-                    className="py-20"
-                    style={{ backgroundImage: "url('/playground-bg.jpg')" }}>
-                    <Playground />
-                </div>
-            </main>
+                                backgroundImage: "url('/playground-bg.jpg')",
+                            }}>
+                            <Playground />
+                        </div>
+                    </main>
+                </motion.div>
+            </AnimatePresence>
         </>
     );
 }
